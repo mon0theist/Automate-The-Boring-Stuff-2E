0765188a35bb8ad67c4a214abb9680c25803364e
mon0theist <bburns91@gmail.com>
2020-05-27 11:10:34 -0500

    Completed Chapter 3 - Collatz Sequence

---
+++ Chapter 3/collatz.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python
+#
+# ATBS - Chapter 3 - Collatz Sequence
+#
+# Write a function named collatz() that has one parameter named number. If number
+# is even, then collatz() should print number // 2 and return this value. If
+# number is odd, then collatz() should print and return 3 * number + 1.
+#
+# Then write a program that lets the user type in an integer and that keeps
+# calling collatz() on that number until the function returns the value 1.
+# (Amazingly enough, this sequence actually works for any integer—sooner or later,
+# using this sequence, you’ll arrive at 1! Even mathematicians aren’t sure why.
+# Your program is exploring what’s called the Collatz sequence, sometimes called
+# “the simplest impossible math problem.”)
+#
+# Remember to convert the return value from input() to an integer with the int()
+# function; otherwise, it will be a string value.
+#
+# Hint: An integer number is even if number % 2 == 0, and it’s odd if
+# number % 2 == 1.
+
+def collatz(number):
+    # even
+    if number % 2 == 0:
+        print(number // 2)
+        return(number // 2)
+    # odd
+    elif number % 2 == 1:
+        print(3 * number + 1)
+        return(3 * number + 1)
+
+try:
+    num = int(input('Please enter an integer to Collatz: '))
+    while num != 1:
+        num = collatz(num)
+except ValueError:
+    print('Error: Please enter an integer')
