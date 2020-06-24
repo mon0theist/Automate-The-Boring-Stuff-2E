719e102f31ae4a46122cac0b8405bf76ea58d2cf
mon0theist <bburns91@gmail.com>
2020-06-24 11:16:21 -0500

    Starting over on coin_flip_streaks.py

--- Chapter 04/coin_flip_streaks.py
+++ Chapter 04/coin_flip_streaks.py
@@ -26,30 +26,11 @@
 
 import random
 
-# 0 = Tails
-# 1 = Heads
-
 numberOfStreaks = 0
 
 for experimentNumber in range(10000):
     # Code that creates a list of 100 'heads' or 'tails' values.
-    flips = []
-    for i in range(100):
-        int = random.randint(0, 1)
-        if int == 0:
-            flips.append('T')
-        elif int == 1:
-            flips.append('H')
 
     # Code that checks if there is a streak of 6 heads or tails in a row.
-    count = 0
-    for i in range(len(flips)):
-        while count < 6:
-            if flips[i] == flips[i-1]:
-                count += 1
-            elif flips[i] != flips[i-1]:
-                count == 0
-    if count == 6:
-        numberOfStreaks += 1
 
 print('Chance of streak: %s%%' % (numberOfStreaks / 100))
